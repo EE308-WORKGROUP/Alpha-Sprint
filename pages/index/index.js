1d423a0e168f2b5c4cd8acfc2e8128a18c285605
why <569097054@qq.com>
2021-11-11 22:59:00 +0800

    origin code

---
+++ pages/index/index.js
@@ -0,0 +1,54 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
+    favorite:{},
+    history:{}
+  },
+  favorite(){
+    wx.navigateTo({
+      url: '../favorite/favorite'
+    })
+  },
+  // 事件处理函数
+  
+  history() {
+    wx.navigateTo({
+      url: '../history/history'
+    })
+  },
+  onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  getUserProfile(e) {
+    wx.getUserProfile({
+      desc: '展示用户信息', 
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
